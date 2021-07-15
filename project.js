7aa6ec08f172a8adbb7f51369fc95f97aef73796
Akriti Chaudhary <akritidpsg11@gmail.com>
2021-07-15 12:16:20 +0530

    Assignment 12

---
+++ project.js
@@ -0,0 +1,7 @@
+document.getElementById("h1").innerHTML=<b>"Welcome to my site!"</b>;
+document.getElementById("p1").innerText=  "My name is Akriti";
+document.getElementById("h2").innerHTML="";
+document.getElementById("p2").innerText="I love JavaScript";
+document.getElementById("p2").style.color="blue";
+
+
